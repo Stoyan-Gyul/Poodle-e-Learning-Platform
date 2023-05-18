4d97371960b02bb4915802f8c05907fd21a4b929
momtchilbotev <botev_m@yahoo.com>
2023-05-18 11:13:37 +0300

    models refactored

--- data/models.py
+++ data/models.py
@@ -103,29 +103,7 @@ class Report(BaseModel):
             status=StatusLevelMaps.INT_TO_STR[status],
             rating=rating,
             progress=progress
-            )from pydantic import BaseModel
-
-class User(BaseModel):
-    id: int | None
-    email: str
-    password: str
-    first_name: str | None
-    last_name: str | None
-    role: str | None
-
-    @classmethod
-    def from_query_result(cls, id, email, password, first_name, last_name, role):
-        return cls(
-            id=id,
-            email=email,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-            role=role)
-
-class LoginData(BaseModel):
-    email: str | None
-    password: str | None
+            )
 
 class TeacherAdds(BaseModel):
     phone_number: str | None
