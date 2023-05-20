562ba75510346873e2f01338598dcc4597e38f7c
Stoyan <stoyan.gyulemerov@gmail.com>
2023-05-20 22:28:16 +0300

    Add courses_router to main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 from routers.users import user_router
+from routers.courses import courses_router
 
 
 
 app = FastAPI()
 app.include_router(user_router)
+app.include_router(courses_router)
 
 
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
