94ef6aca9ed266d5d4f1526c009af9a90c6f2b76
Stoyan <stoyan.gyulemerov@gmail.com>
2023-05-20 16:19:40 +0300

    Add get_user_or_raise_401 to auth.py

--- data/common/auth.py
+++ data/common/auth.py
@@ -1,4 +1,5 @@
-from services.users_service import validate_token
+from data.models import User
+from services.users_service import find_by_id, validate_token
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 
@@ -9,4 +10,11 @@ def get_user_params_or_raise_error(token: str) -> list:
         raise HTTPException(status_code=401, detail="Problem with the authentication. Try Again!")
 
     return token_params
-    
+
+
+def get_user_or_raise_401(token: str) -> User:
+    token_params = get_user_params_or_raise_error(token)
+    user_id = token_params[0]
+    user = find_by_id(user_id)
+
+    return user
