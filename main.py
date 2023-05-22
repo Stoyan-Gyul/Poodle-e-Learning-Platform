2d2cda83b71d858c3e0ebeb5c01323ab0046d156
teodora stoykowa <t.stoykowa@gmail.com>
2023-05-22 12:23:31 +0300

    corsmiddleware

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import uvicorn
 from fastapi import FastAPI
 from routers.users import user_router
-from routers.courses import courses_router
-
+#from routers.courses import courses_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+
 app.include_router(user_router)
-app.include_router(courses_router)
+#app.include_router(courses_router)
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with the appropriate list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Replace with the appropriate list of allowed HTTP methods
+    allow_headers=["*"],  # Replace with the appropriate list of allowed headers
+)
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
