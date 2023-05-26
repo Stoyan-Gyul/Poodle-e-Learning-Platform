12c0254e36e014a01e390889dca738bd7b403744
Stoyan <stoyan.gyulemerov@gmail.com>
2023-05-26 15:58:54 +0300

    Update get_user_or_raise_401 for Bearer token use

--- data/common/auth.py
+++ data/common/auth.py
@@ -2,6 +2,7 @@ from data.models import User
 from services.users_service import find_by_id, validate_token
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
+from fastapi import status
 
 
 def get_user_params_or_raise_error(token: str) -> list:
@@ -12,8 +13,13 @@ def get_user_params_or_raise_error(token: str) -> list:
     return token_params
 
 
-def get_user_or_raise_401(token: str) -> User:
-    token_params = get_user_params_or_raise_error(token)
+def get_user_or_raise_401(authorization: str) -> User:
+    token = authorization.split(" ")[1] if authorization.startswith("Bearer ") else None
+
+    token_params=validate_token(token)
+    if not token_params:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Problem with the authentication. Try Again!")
+    
     user_id = token_params[0]
     user = find_by_id(user_id)
 
