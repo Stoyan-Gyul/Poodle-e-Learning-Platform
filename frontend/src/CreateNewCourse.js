b8f39b2a4c7817b93bfc9e045b8137212b7b0712
teodora stoykowa <t.stoykowa@gmail.com>
2023-05-29 22:03:13 +0300

    create new course page added

---
+++ frontend/src/CreateNewCourse.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateCoursePage = () => {
+  return (
+    <div>
+      <h1>Create New Course</h1>
+      {/* Add the form and input fields for creating a new course */}
+    </div>
+  );
+};
+
+export default CreateCoursePage;
