71882730781c28c7008398152df26f56cd45202e
teodora stoykowa <t.stoykowa@gmail.com>
2023-05-29 22:02:28 +0300

    login and signUp api requests

--- frontend/src/API_requests.js
+++ frontend/src/API_requests.js
@@ -1,7 +1,55 @@
 
 
+const authToken = localStorage.getItem('authToken');
+
+export const apiLogin = async (email, password) => {
+  // Make the POST request to your backend API
+  const response = await fetch('http://localhost:8000/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    const token = data.token;
+    return token;
+  } else {
+    throw new Error('Login failed');
+  }
+};
+
+export const signup = async (userData) => {
+  try {
+    const response = await fetch('http://localhost:8000/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (response.ok) {
+      // Signup successful
+      return;
+    } else {
+      // Signup failed
+      // Handle the error and throw an appropriate error
+      const errorData = await response.json();
+      const errorMessage = errorData.detail || 'Signup failed';
+      throw new Error(errorMessage);
+    }
+  } catch (error) {
+    // Handle any network or server errors
+    console.error('Error occurred while signing up:', error);
+    throw new Error('Signup failed');
+  }
+};
+
+
 export const handleUnsubscribeFromCourse = async (courseId) => {
-    const authToken = localStorage.getItem('authToken');
     const user_id = 57
     
     try {
@@ -23,9 +71,7 @@ export const handleUnsubscribeFromCourse = async (courseId) => {
     }
   };
 
-
 export const fetchEnrolledCourses = async () => {
-  const authToken = localStorage.getItem('authToken');
 
   try {
     const response = await fetch('http://localhost:8000/courses/enrolled_courses', {
@@ -50,3 +96,53 @@ export const fetchEnrolledCourses = async () => {
     throw error;
   }
 };
+
+export const fetchAllCourses = async () => {
+  try {
+    const response = await fetch(`http://localhost:8000/courses/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch courses');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error fetching courses:', error);
+    throw error;
+  }
+};
+
+export const createCourse = async (course) => {
+  try {
+    const response = await fetch(`http://localhost:8000/courses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(course),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to create course');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error creating course:', error);
+    throw error;
+  }
+};
+
+
+
+
+
+
