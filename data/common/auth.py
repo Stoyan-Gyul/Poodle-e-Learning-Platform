4e457c598747c361192704a0e07438e2f94251cb
momtchilbotev <botev_m@yahoo.com>
2023-05-30 14:25:27 +0300

    is_user_approved refactored

--- data/common/auth.py
+++ data/common/auth.py
@@ -29,7 +29,7 @@ def get_user_or_raise_401(authorization: str) -> User:
 
 def is_user_approved_by_admin(user_id: int)->bool:
     ''' Verify is user role is approved'''
-    sql='''SELECT is_verified FROM users WHERE id=?'''
+    sql='''SELECT is_approved FROM users WHERE id=?'''
     if read_query(sql,(user_id,))[0][0]:
         return True
     return False
