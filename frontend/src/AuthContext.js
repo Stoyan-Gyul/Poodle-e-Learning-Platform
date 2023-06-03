1b8ebbeac9b700f7a3d95ed87fcd7bfc002f7160
teodora stoykowa <t.stoykowa@gmail.com>
2023-06-04 00:07:55 +0300

    logout implemented

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useContext } from 'react';
+import { UserContext } from './UserContext';
 
 // Create the AuthContext
 export const AuthContext = createContext();
@@ -6,14 +7,28 @@ export const AuthContext = createContext();
 // Create the AuthProvider component
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState('');
+  const { setUserId, setRole} = useContext(UserContext)
 
   // Function to set the token
   const setAuthToken = (newToken) => {
     setToken(newToken);
   };
+
+  const logout = () => {
+    // Remove the token from local storage
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('role');
+
+    // Clear the token, user_id, and role from the app state
+    setAuthToken('');
+    setUserId('');
+    setRole('');
+  };
+
   console.log(token);
   return (
-    <AuthContext.Provider value={{ token, setAuthToken }}>
+    <AuthContext.Provider value={{ token, setAuthToken, logout }}>
       {children}
     </AuthContext.Provider>
   );
