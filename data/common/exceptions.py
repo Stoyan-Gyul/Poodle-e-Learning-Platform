13a085691159b55023202a032121907a2de7c187
Stoyan <stoyan.gyulemerov@gmail.com>
2023-06-04 14:14:17 +0300

    Update exceptions in auth

--- data/common/exceptions.py
+++ data/common/exceptions.py
@@ -9,6 +9,10 @@ class Exception400BadRequest(CustomHTTPException):
     def __init__(self, detail: str = None):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
 
+class Exception401Unauthorized(CustomHTTPException):
+    def __init__(self, detail: str = None):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
+
 class Exception403Forbidden(CustomHTTPException):
     def __init__(self, detail: str = None):
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
